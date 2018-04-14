de7f2279e389a0494a2b565dce29ca1e605b22d8
cristian-pulido <cpulido@unal.edu.co>
2018-04-14 17:05:49 -0500

    permisos eliminacion

--- apps/fileupload/templatetags/upload_tags.py
+++ apps/fileupload/templatetags/upload_tags.py
@@ -4,72 +4,140 @@ from django.utils.safestring import mark_safe
 
 register = template.Library()
 
+
 @register.simple_tag
-def upload_js():
-    return mark_safe("""
-<!-- The template to display files available for upload -->
-<script id="template-upload" type="text/x-tmpl">
-{% for (var i=0, file; file=o.files[i]; i++) { %}
-    <tr class="template-upload fade">
-        
-        <td>
-            <p class="name">{%=file.name%}</p>
-            {% if (file.error) { %}
-                <div><span class="label label-important">{%=locale.fileupload.error%}</span> {%=file.error%}</div>
-            {% } %}
-        </td>
-        <td>
-            <p class="size">{%=o.formatFileSize(file.size)%}</p>
-            {% if (!o.files.error) { %}
-                <div class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0"><div class="progress-bar progress-bar-success" style="width:0%;"></div></div>
-            {% } %}
-        </td>
-        
-        <td>
-            {% if (!o.files.error && !i && !o.options.autoUpload) { %}
-                <button class="btn btn-primary start">
-                    <i class="glyphicon glyphicon-upload"></i>
-                    <span>{%=locale.fileupload.start%}</span>
-                </button>
-            {% } %}
-            
-            
-        </td>
-        
-    </tr>
-{% } %}
-</script>
-<!-- The template to display files{%=file.name%} available for download -->
-<script id="template-download" type="text/x-tmpl">
-{% for (var i=0, file; file=o.files[i]; i++) { %}
-    <tr class="template-download fade">
-        
-        <td>
-            <p class="name">
-               <a href={%=file.url%}>{%=file.name%}</a>
-            </p>
-            {% if (file.error) { %}
-                <div><span class="label label-important">{%=locale.fileupload.error%}</span> {%=file.error%}</div>
-            {% } %}
-        </td>
-                       
-        <td>
-            <span class="size">{%=o.formatFileSize(file.size)%}</span>
-        </td> 
-        <td>
-            <button class="btn btn-danger delete" data-type="{%=file.deleteType%}" data-url="{%=file.deleteUrl%}"{% if (file.deleteWithCredentials) { %} data-xhr-fields='{"withCredentials":true}'{% } %}>
-                <i class="glyphicon glyphicon-trash"></i>
-                <span>{%=locale.fileupload.destroy%}</span>
-            </button>
-            
-        </td>
-        <td>
-           <p style={%=file.color%} class="Verificacion" >{%=file.verificacion%}</p>  
-        </td>
-    </tr>
-{% } %}
-</script>
-""")
+def upload_js(per):
+    if per==True:
+        return mark_safe("""
+        <!-- The template to display files available for upload -->
+        <script id="template-upload" type="text/x-tmpl">
+        {% for (var i=0, file; file=o.files[i]; i++) { %}
+            <tr class="template-upload fade">
+
+                <td>
+                    <p class="name">{%=file.name%}</p>
+                    {% if (file.error) { %}
+                        <div><span class="label label-important">{%=locale.fileupload.error%}</span> {%=file.error%}</div>
+                    {% } %}
+                </td>
+                <td>
+                    <p class="size">{%=o.formatFileSize(file.size)%}</p>
+                    {% if (!o.files.error) { %}
+                        <div class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0"><div class="progress-bar progress-bar-success" style="width:0%;"></div></div>
+                    {% } %}
+                </td>
+
+                <td>
+                    {% if (!o.files.error && !i && !o.options.autoUpload) { %}
+                        <button class="btn btn-primary start">
+                            <i class="glyphicon glyphicon-upload"></i>
+                            <span>{%=locale.fileupload.start%}</span>
+                        </button>
+                    {% } %}
+
+
+                </td>
+
+            </tr>
+        {% } %}
+        </script>
+        <!-- The template to display files{%=file.name%} available for download -->
+        <script id="template-download" type="text/x-tmpl">
+        {% for (var i=0, file; file=o.files[i]; i++) { %}
+            <tr class="template-download fade">
+
+                <td>
+                    <p class="name">
+                       <a href={%=file.url%}>{%=file.name%}</a>
+                    </p>
+                    {% if (file.error) { %}
+                        <div><span class="label label-important">{%=locale.fileupload.error%}</span> {%=file.error%}</div>
+                    {% } %}
+                </td>
+
+                <td>
+                    <span class="size">{%=o.formatFileSize(file.size)%}</span>
+                </td> 
+                <td>
+                    <button class="btn btn-danger delete" data-type="{%=file.deleteType%}" data-url="{%=file.deleteUrl%}"{% if (file.deleteWithCredentials) { %} data-xhr-fields='{"withCredentials":true}'{% } %}>
+                        <i class="glyphicon glyphicon-trash"></i>
+                        <span>{%=locale.fileupload.destroy%}</span>
+                    </button>
+
+                </td>
+
+                <td>
+                   <p style={%=file.color%} class="Verificacion" >{%=file.verificacion%}</p>  
+                </td>
+            </tr>
+        {% } %}
+
+        </script>  
+        """)
+    else:
+        return mark_safe("""
+        <!-- The template to display files available for upload -->
+        <script id="template-upload" type="text/x-tmpl">
+        {% for (var i=0, file; file=o.files[i]; i++) { %}
+            <tr class="template-upload fade">
+
+                <td>
+                    <p class="name">{%=file.name%}</p>
+                    {% if (file.error) { %}
+                        <div><span class="label label-important">{%=locale.fileupload.error%}</span> {%=file.error%}</div>
+                    {% } %}
+                </td>
+                <td>
+                    <p class="size">{%=o.formatFileSize(file.size)%}</p>
+                    {% if (!o.files.error) { %}
+                        <div class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0"><div class="progress-bar progress-bar-success" style="width:0%;"></div></div>
+                    {% } %}
+                </td>
+
+                <td>
+                    {% if (!o.files.error && !i && !o.options.autoUpload) { %}
+                        <button class="btn btn-primary start">
+                            <i class="glyphicon glyphicon-upload"></i>
+                            <span>{%=locale.fileupload.start%}</span>
+                        </button>
+                    {% } %}
+
+
+                </td>
+
+            </tr>
+        {% } %}
+        </script>
+        <!-- The template to display files{%=file.name%} available for download -->
+        <script id="template-download" type="text/x-tmpl">
+        {% for (var i=0, file; file=o.files[i]; i++) { %}
+            <tr class="template-download fade">
+
+                <td>
+                    <p class="name">
+                       <a href={%=file.url%}>{%=file.name%}</a>
+                    </p>
+                    {% if (file.error) { %}
+                        <div><span class="label label-important">{%=locale.fileupload.error%}</span> {%=file.error%}</div>
+                    {% } %}
+                </td>
+
+                <td>
+                    <span class="size">{%=o.formatFileSize(file.size)%}</span>
+                </td> 
+                <td>
+                   <p style={%=file.color%} class="Verificacion" >{%=file.verificacion%}</p>  
+                </td>
+            </tr>
+        {% } %}
+
+        </script>
+        """)
+
+
+
+
+
 
 
 
