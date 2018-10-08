45eb542620dd2a57d70120c1ac8965cdf82b9bd0
cristian-pulido <cpulido@unal.edu.co>
2018-10-07 20:03:08 -0500

    7oct

--- programas/definitions.py
+++ programas/definitions.py
@@ -6,4 +6,4 @@ typeimg = ["4D", "3D"]
 folder_data = "/home/runlab/data/sanjose"
 folder_data_types = ["controls", "patients"]
 
-templete_T1="/usr/share/fsl/data/standard/MNI152_T1_2mm.nii.gz"
+templete_T1=os.path.join(os.environ['FSLDIR'], 'data/standard/MNI152_T1_2mm.nii.gz')
