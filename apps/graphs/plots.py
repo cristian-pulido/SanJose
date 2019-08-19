de25f4f03a6f9d788b5e951b42d6e71439b8b7cb
Cristian Pulido <cpulido@unal.edu.co>
2019-08-19 02:15:46 -0500

    19ago

--- apps/graphs/plots.py
+++ apps/graphs/plots.py
@@ -7,6 +7,7 @@ import requests
 from apps.paciente.models import Candidato
 import numpy as np
 import plotly.express as px
+import os
 
 
 
@@ -125,6 +126,57 @@ def motivo_exclusion():
 
 	return plot_div
 
+def D_previo_hist():
+	C=Candidato.objects.filter(inscrito=False)
+	D=[]
+	for c in C:
+		d_previo=list(c.D_neuro_logico_psiquiatrico_previo.all().values_list('nombre',flat=True))
+		if len(d_previo) > 0:
+			D+=d_previo
+            
+	labels,frec=np.unique(D,return_counts=True)
+	count_sort_ind = np.argsort(-frec)
+	
+	labels=labels[count_sort_ind]
+	frec=frec[count_sort_ind]
+	percent=np.around(frec/frec.sum(),2)
+	            
+	fig = go.Figure(data=[go.Bar(x=labels, y=frec,
+                                 text=frec,
+                                 textposition='auto',
+                                 hovertext=[str(p*100)+"%" for p in percent])])   
+    
+	fig['layout'].update(legend=dict(orientation="h"))
+	plot_div = plot(fig, output_type='div',filename='D_previo_hist')
+
+	return plot_div
+
+def D_especifico():
+	from django.conf import settings
+	from wordcloud import WordCloud
+	import matplotlib as mpl
+	import matplotlib.pyplot as plt
+	C=Candidato.objects.filter(inscrito=False)
+	especificos=list(C.values_list('D_especificos',flat=True))
+	especificos=[i for i in especificos if i !='']
+	W=' '.join(especificos)
+	W=W.replace('de', '').replace('Antecedente', '').replace('en', '')
+	wordcloud = WordCloud(
+                          background_color='white',
+                          max_words=200,
+                          max_font_size=40, 
+                          width=400, height=250,
+                          random_state=42
+                         ).generate(W)
+	plt.figure(figsize=(8,6))    
+	plt.imshow(wordcloud)
+	plt.axis('off')
+	media=settings.MEDIA_ROOT
+	plt.savefig(os.path.join(media,'wordcloud_D_Especifico.png'),dpi=1500)
+    
+
+	return os.path.join("/media/",'wordcloud_D_Especifico.png')
+
 def demografico_continuo():
 	C=Candidato.objects.filter(inscrito=True)
 	edad=C.values_list('edad',flat=True)
@@ -135,7 +187,7 @@ def demografico_continuo():
 	continuo_values=list(T.edad.values)+list(T.peso.values)+list(T.estatura.values)
 	continuo_variable=["Edad"]*len(T)+["Peso (Kgs)"]*len(T)+["Estatura (cms)"]*len(T)
 	continuo=pd.DataFrame([continuo_values,continuo_variable],index=['valor','Variable']).T
-	fig = px.box(continuo,y='valor',color='Variable')
+	fig = px.violin(continuo,y='valor',color='Variable',box=True,points='all')
 	plot_div = plot(fig, output_type='div',filename='demografico_continuo')
 
 	return plot_div
@@ -244,6 +296,62 @@ def clinica_conciencia():
   plot_div = plot(fig, output_type='div',filename='clinica_conciencia')
   return plot_div
 
+def clinica_a_patologicos():
+	from apps.paciente.models import Ingreso    
+	C=Ingreso.objects.all()
+	P=[]
+	for c in C:
+		patologico=list(c.a_patologicos.all().values_list('nombre',flat=True))
+		if len(patologico) > 0:
+			P+=patologico
+            
+	labels,frec=np.unique(P,return_counts=True)
+	count_sort_ind = np.argsort(-frec)
+	
+	labels=labels[count_sort_ind]
+	frec=frec[count_sort_ind]
+	percent=np.around(frec/frec.sum(),2)
+	            
+	fig = go.Figure(data=[go.Bar(x=labels, y=frec,
+                                 text=frec,
+                                 textposition='auto',
+                                 hovertext=[str(p*100)+"%" for p in percent])])   
+    
+	fig['layout'].update(legend=dict(orientation="h"))
+	plot_div = plot(fig, output_type='div',filename='clinica_a_patologicos')
+
+	return plot_div
+
+def clinica_antecedentes():
+	from django.conf import settings
+	from wordcloud import WordCloud
+	import matplotlib as mpl
+	import matplotlib.pyplot as plt
+	from apps.paciente.models import Ingreso    
+	I=Ingreso.objects.all()
+	antecedentes = ['a_patologicos_cual','a_toxico_alergenicos','a_familiares']
+	for i in antecedentes:
+		W=" ".join([i for i in list(I.values_list(i,flat=True)) if i])
+		wordcloud = WordCloud(
+                              background_color='white',
+                              max_words=200,
+                              max_font_size=40, 
+                              width=400, height=250,
+                              random_state=42
+                             ).generate(W)
+		plt.figure(figsize=(8,6))    
+		plt.imshow(wordcloud)
+		plt.axis('off')
+		media=settings.MEDIA_ROOT
+		plt.savefig(os.path.join(media,'wordcloud_'+i+'.png'),dpi=1500)
+    
+
+	return [os.path.join("/media/",'wordcloud_'+i+'.png') for i in antecedentes]
+
+
+
+
+
 def clinica_uci_g():
   C=Candidato.objects.filter(inscrito=True)
   glasgow= [c.uci.glasgowtotal if hasattr(c,'uci') else None for c in C]
@@ -277,6 +385,127 @@ def clinica_formularios():
 
   plot_div = plot(fig, output_type='div',filename='clinica_formularios')
   return plot_div
+
+def uci_glasgow_ingreso():
+    from apps.paciente.models import Uci
+    U=Uci.objects.filter(candidato__inscrito=True)
+    variables=[['Apertura ocular','apertura_ocular'],['Respuesta motora','respuesta_motora'],['Respuesta Verbal','respuesta_verbal'],['Glasgow Total ingreso','glasgowtotal']]
+    
+    A=pd.DataFrame()
+    for i in variables:
+        A[i[0]]=U.values_list(i[1],flat=True)
+        
+    fig=px.parallel_categories(A,dimensions=[v[0] for v in variables])#,color='Glasgow Total ingreso')
+    
+    plot_div = plot(fig, output_type='div',filename='uci_glasgow_ingreso')
+    return plot_div
+
+def uci_variables_vs_egreso():
+    from apps.paciente.models import Uci
+    U=Uci.objects.filter(candidato__inscrito=True)
+    variables=[['fallaorganica','Falla Organica'],['sepsis','Sepsis'],['presion_intracraneana','Presión Intracraneana'],['monitoria_pic','Monitoria Pic'],['ventilacionmecanica','Ventilación Mecanica'],['soportevasopresor','Soporte Vasopresor'],['condicion_egreso','Condición Egreso']]
+    
+    A=pd.DataFrame()
+    for i in variables:
+        A[i[1]]=U.values_list(i[0],flat=True)
+        
+    fig=px.parallel_categories(A,dimensions=[v[1] for v in variables])
+    
+    plot_div = plot(fig, output_type='div',filename='uci_variables_vs_egreso')
+    return plot_div
+
+def uci_falla_organica():
+	from apps.paciente.models import Uci   
+	U=Uci.objects.filter(candidato__inscrito=True)
+	F=[i for i in list(U.values_list('fallaorganica_cual',flat=True)) if i]
+
+            
+	labels,frec=np.unique(F,return_counts=True)
+	count_sort_ind = np.argsort(-frec)
+	
+	labels=labels[count_sort_ind]
+	frec=frec[count_sort_ind]
+	percent=np.around(frec/frec.sum(),2)
+	            
+	fig = go.Figure(data=[go.Bar(x=labels, y=frec,
+                                 text=frec,
+                                 textposition='auto',
+                                 hovertext=[str(p*100)+"%" for p in percent])])   
+    
+	fig['layout'].update(legend=dict(orientation="h"))
+	plot_div = plot(fig, output_type='div',filename='uci_falla_organica')
+
+	return plot_div
+
+def uci_glasgow_egreso():
+    from apps.paciente.models import Uci
+    U=Uci.objects.filter(candidato__inscrito=True)
+    variables=[['Apertura ocular','apertura_ocular_e'],['Respuesta motora','respuesta_motora_e'],['Respuesta Verbal','respuesta_verbal_e'],['Glasgow Total Egreso','glasgowtotal_e']]
+    
+    A=pd.DataFrame()
+    for i in variables:
+        A[i[0]]=U.values_list(i[1],flat=True)
+        
+    fig=px.parallel_categories(A,dimensions=[v[0] for v in variables])#,color='Glasgow Total ingreso')
+    
+    plot_div = plot(fig, output_type='div',filename='uci_glasgow_egreso')
+    return plot_div
+
+def uci_causa_mortalidad():
+	from apps.paciente.models import Uci   
+	U=Uci.objects.filter(candidato__inscrito=True)
+	F=[i for i in list(U.values_list('causa_mortalidad',flat=True)) if i]
+    
+	for i in range(len(F)):
+		F[i]=F[i].lower()
+
+            
+	labels,frec=np.unique(F,return_counts=True)
+	count_sort_ind = np.argsort(-frec)
+	
+	labels=labels[count_sort_ind]
+	frec=frec[count_sort_ind]
+	percent=np.around(frec/frec.sum(),2)
+	            
+	fig = go.Figure(data=[go.Bar(x=labels, y=frec,
+                                 text=frec,
+                                 textposition='auto',
+                                 hovertext=[str(p*100)+"%" for p in percent])])   
+    
+	fig['layout'].update(legend=dict(orientation="h"))
+	plot_div = plot(fig, output_type='div',filename='uci_causa_mortalidad')
+
+	return plot_div
+
+def uci_dx_egreso():
+	from django.conf import settings
+	from wordcloud import WordCloud
+	import matplotlib as mpl
+	import matplotlib.pyplot as plt
+	from apps.paciente.models import Uci   
+	U=Uci.objects.filter(candidato__inscrito=True)
+
+
+	W=" ".join([i for i in list(U.values_list('dx_egreso',flat=True)) if i])
+	wordcloud = WordCloud(
+                              background_color='white',
+                              max_words=200,
+                              max_font_size=40, 
+                              width=400, height=250,
+                              random_state=42
+                             ).generate(W)
+	plt.figure(figsize=(8,6))    
+	plt.imshow(wordcloud)
+	plt.axis('off')
+	media=settings.MEDIA_ROOT
+	plt.savefig(os.path.join(media,'wordcloud_uci_dx_egreso.png'),dpi=1500)
+    
+
+	return os.path.join("/media/",'wordcloud_uci_dx_egreso.png')
+
+
+
+
 
 def neuropsi_vs_G_diagnostico():
   from apps.paciente.models import Neuropsi
--- apps/graphs/views.py
+++ apps/graphs/views.py
@@ -9,8 +9,9 @@ class graphs_dash(TemplateView):
         context['num_sujetos']=plots.num_sujetos()
         context['time_line_sujetos']=plots.time_line_sujetos()
         context['motivo_exclusion']=plots.motivo_exclusion()
-
-        
+        context['D_previo_hist']=plots.D_previo_hist()
+        context['D_especifico']=plots.D_especifico()
+              
 
         
         
@@ -37,7 +38,12 @@ class graphs_clinica(TemplateView):
         context['clinica_conciencia']=plots.clinica_conciencia()
         context['clinica_uci_g']=plots.clinica_uci_g()
         context['clinica_uci_egreso']=plots.clinica_uci_egreso()
-        context['clinica_formularios']=plots.clinica_formularios()      
+        context['clinica_formularios']=plots.clinica_formularios()   
+        context['clinica_a_patologicos']=plots.clinica_a_patologicos()   
+        context['clinica_a_patologicos_cual'],context['a_toxico_alergenicos'],context['a_familiares']=plots.clinica_antecedentes()   
+        
+        
+        
 
         return context
 
@@ -45,8 +51,13 @@ class graphs_uci(TemplateView):
     template_name = 'graphs/uci.html'
     def get_context_data(self, **kwargs):
         context = super(graphs_uci, self).get_context_data(**kwargs)
+        context['uci_glasgow_ingreso']=plots.uci_glasgow_ingreso()   
+        context['uci_variables_vs_egreso']=plots.uci_variables_vs_egreso() 
+        context['uci_falla_organica']=plots.uci_falla_organica() 
+        context['uci_glasgow_egreso']=plots.uci_glasgow_egreso() 
+        context['uci_causa_mortalidad']=plots.uci_causa_mortalidad() 
+        context['uci_dx_egreso']=plots.uci_dx_egreso()     
         
-
         return context
 
 class graphs_neuropsicologia(TemplateView):
